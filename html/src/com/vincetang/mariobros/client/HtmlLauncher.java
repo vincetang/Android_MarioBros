4590fa32d036df874352cfab23c17cb78ef41e3a
Vincent Tang <Vince@MacBook-Pro.local>
2016-06-26 20:42:17 -0400

    Initial commit

---
+++ html/src/com/vincetang/mariobros/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.vincetang.mariobros.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.vincetang.mariobros.MarioBros;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new MarioBros();
+        }
+}
